beea230826ec71f677932c98b608d9e5f021d4b2
MyungHoon Jin <jinmang2@gmail.com>
2020-12-31 10:17:19 +0900

    Update bpmll.py
    배치 연산이 가능하게 수정
    속도 개선은 측정해봐야함

--- BPMLL/bpmll.py
+++ BPMLL/bpmll.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
 
 
 class Model(nn.Module):
@@ -19,17 +20,39 @@ class BPMLLLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, input, target):
+    def forward(self, input, target, batch_first=True):
         # https://cs.nju.edu.cn/zhouzh/zhouzh.files/publication/tkde06a.pdf
         # input's shape is (bsz, Q+2)
         # target's shape is (bsz, Q+2)
         # target's unique value is in [-1, 1]
-        loss_sum = 0
-        for batch_ix, mask in enumerate(target):
-            Y = input[batch_ix][mask == 1]
-            Y_bar = input[batch_ix][mask != 1]
-            k = len(Y) * len(Y_bar)
-            term = -torch.cartesian_prod(Y, -Y_bar).sum(dim=-1)
-            loss = (1 / k) * torch.sum(torch.exp(term))
-            loss_sum += loss
-        return loss_sum
+        
+        # get a batch size
+        ix = 0 if batch_first else -1
+        bsz = target.size(ix)
+        
+        # get true and false labels
+        y_i = (target == 1)
+        y_i_bar = (target != 1)
+        
+        # get indices to check
+        truth_matrix = torch.logical_and(y_i.unsqueeze(2), y_i_bar.unsqueeze(1))
+        
+        # calculate all exponential diff each batches
+        exp_matrix = torch.exp(output.unsqueeze(1) - output.unsqueeze(2))
+        
+        # get normalizing term
+        length = truth_matrix.sum(dim=[1,2])
+        
+        # padding and sum per batches
+        batch_losses = pad_sequence(
+            # For batch calculate, do pad by 0 values
+            exp_matrix[truth_matrix].split(tuple(length.numpy()))
+        ).sum(dim=0)
+        
+        # normalizing
+        losses = batch_losses / y_norm
+        
+        # calculate loss
+        loss = (losses / bsz).sum()
+        
+        return loss

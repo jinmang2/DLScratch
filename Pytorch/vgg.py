7dbbbf6da7e78339aafa7364ed0df1f2dd70d82e
MyungHoon Jin <jinmang2@gmail.com>
2021-01-25 16:59:18 +0900

    Update vgg.py
    Add hook

--- Pytorch/vgg.py
+++ Pytorch/vgg.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn
-from typing import List, Optional, Union
+from torch.utils.hooks import RemovableHandle
+from typing import List, Optional, Union, Tuple, Callable, Dict
 from copy import copy
 
 
@@ -10,11 +11,16 @@ class VGG(nn.Module):
     
     def __init__(
         self, 
-        in_channels:int = 3, 
-        n_layers_per_block:List[int] = [2, 2, 4, 4, 4],
-        n_channels_per_block:List[int] = [64, 128, 256, 512, 512]
+        in_channels: int=3, 
+        n_layers_per_block: List[int]=[2, 2, 4, 4, 4],
+        n_channels_per_block: List[int]=[64, 128, 256, 512, 512],
+        pool: str='max',
     ):
         super().__init__()
+        
+        assert len(n_layers_per_block) == len(n_channels_per_block)
+        self.num_blocks = len(n_layers_per_block)
+        
         loop = zip(n_layers_per_block, n_channels_per_block)
         for ix, (block, channels) in enumerate(loop, start=1):
             out_channels = channels
@@ -22,24 +28,44 @@ class VGG(nn.Module):
                 if block_ix == 2: in_channels = channels
                 setattr(self, f'conv{ix}_{block_ix}',
                     nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1))
-            setattr(self, f'pool{ix}', nn.MaxPool2d(kernel_size=2, stride=2))
-            in_channels = out_channels
-        self.valid_name = [name for name, _ in self.named_children()]
-                   
-    def forward(self, x: torch.Tensor, inp: Union[str, List]=[]):
-        _inp = self._check_valid_inp(inp)
-        out = {}
-        for name, layer in self.named_children():
+            pool = 'MaxPool2d' if pool.lower() == 'max' else 'AvgPool2d'
+            setattr(self, f'pool{ix}', getattr(nn, pool)(kernel_size=2, stride=2))
+            in_channels = out_channels        
+        
+    def forward(self, x: torch.Tensor):
+        for layer in self.children():
             x = layer(x)
             if isinstance(layer, nn.Conv2d):
                 x = torch.relu(x)
-            if name in _inp:
-                out[name] = x
-                _inp.remove(name)
-            if len(_inp) == 0:
-                break
-        return [out[key] for key in inp]
-               
+        return x
+
+            
+class FeatureExtractor(nn.Module):
+    
+    # https://medium.com/the-dl/how-to-use-pytorch-hooks-5041d777f904
+    
+    def __init__(self, model: nn.Module):
+        super().__init__()
+        self.model = model
+        self.valid_name = [name for name, _ in self.model.named_children()]
+
+    def save_outputs_hook(self, layer_id: str) -> Callable:
+        def fn(_, __, output):
+            self._features[layer_id] = output
+        self.handle[layer_id].remove()
+        return fn
+      
+    def forward(self, x: torch.Tensor, layers: List[str]) -> Dict[str, torch.Tensor]:
+        self._check_valid_inp(layers)
+        self.handle = {layer_id: RemovableHandle(lambda x: _) for layer_id in layers}
+        self._features = {layer_id: torch.empty(0) for layer_id in layers}
+        for layer_id in layers:
+            layer = dict(self.model.named_children())[layer_id]
+            hook = self.save_outputs_hook(layer_id)
+            self.handle[layer_id] = layer.register_forward_hook(hook)
+        _ = self.model(x)
+        return [self._features[layer_id] for layer_id in layers]
+    
     def _check_valid_inp(self, inp):
         if isinstance(inp, str):
             self._is_in_layers(inp)
@@ -49,8 +75,6 @@ class VGG(nn.Module):
                 self._is_in_layers(i)
         else:
             raise AttributeError(f'type(inp): str or list')
-        if len(inp) == 0:
-            inp.append(self.valid_name[-1]) # final layer
         return copy(inp) # from copy import copy
     
     def _is_in_layers(self, name):
